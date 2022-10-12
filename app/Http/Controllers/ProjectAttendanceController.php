b1264a11e8704a21777d10f4db33e66d06f642c7
ibal <muammadibal@gmail.com>
2022-10-12 18:05:10 +0700

    first commit

---
+++ app/Http/Controllers/ProjectAttendanceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProjectAttendance;
+use Illuminate\Http\Request;
+
+class ProjectAttendanceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ProjectAttendance  $projectAttendance
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProjectAttendance $projectAttendance)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ProjectAttendance  $projectAttendance
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProjectAttendance $projectAttendance)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ProjectAttendance  $projectAttendance
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProjectAttendance $projectAttendance)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ProjectAttendance  $projectAttendance
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProjectAttendance $projectAttendance)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/ProjectOvertimeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProjectOvertime;
+use Illuminate\Http\Request;
+
+class ProjectOvertimeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ProjectOvertime  $projectOvertime
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProjectOvertime $projectOvertime)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ProjectOvertime  $projectOvertime
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProjectOvertime $projectOvertime)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ProjectOvertime  $projectOvertime
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProjectOvertime $projectOvertime)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ProjectOvertime  $projectOvertime
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProjectOvertime $projectOvertime)
+    {
+        //
+    }
+}
